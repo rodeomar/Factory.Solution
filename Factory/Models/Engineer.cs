41e5fd687b6b6fc9fe2921fa16567297dfef42b1
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-05 22:20:01 +0530

    Added Engineer Model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Reflection.PortableExecutable;
+
+namespace Factory.Models
+{
+    public class Engineer
+    {
+        public string Name { get; set; }
+        public string LicenseDetails { get; set; }
+        public List<Machine> Machines { get; set; }
+
+
+        public Engineer(string Name, string licenseDetails)
+        {
+
+            this.Name = Name;
+
+            this.Machines = new();
+            this.LicenseDetails = licenseDetails;
+        }
+
+
+    }
+}
