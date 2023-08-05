0f7d9dd9b1ba781b3b66bf3a11e5fa91f21ad6ac
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-05 22:57:55 +0530

    Added one more property(EngineerID) in engineer Model, and updated the INdex viewo of EngineersController

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -6,7 +6,7 @@ namespace Factory.Controllers
     public class EngineersController : Controller
     {
 
-        private static List<Engineer> Engineers = new (){ new Engineer("Raed", "ASDU1312736"), new Engineer("Ammar", "YAGD12837912") };
+        private static List<Engineer> Engineers = new (){ new Engineer("Raed", "ASDU1312736", 1), new Engineer("Ammar", "YAGD12837912", 2) };
 
        public IActionResult Index()
         {
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,16 +6,17 @@ namespace Factory.Models
     {
         public string Name { get; set; }
         public string LicenseID{ get; set; }
+        public int EngineerID { get; set; }
         public List<Machine> Machines { get; set; }
 
 
-        public Engineer(string Name, string licenseID)
+        public Engineer(string Name, string licenseID, int engineerID)
         {
 
             this.Name = Name;
-
             this.Machines = new();
             this.LicenseID = licenseID;
+            this.EngineerID = engineerID;
         }
 
 
