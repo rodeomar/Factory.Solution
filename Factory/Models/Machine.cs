9d63eb2ee9beca7e1e24fa4ac669c90e67c88b9f
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-05 22:20:23 +0530

    Added Machine Model

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,1 +1,18 @@
-﻿
+﻿namespace Factory.Models
+{
+    public class Machine
+    {
+
+        public int MachineId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Machine(int ID, string Name, string description)
+        {
+            this.MachineId = ID;
+            this.Name = Name;
+            this.Description = description;
+        }
+
+    }
+}
