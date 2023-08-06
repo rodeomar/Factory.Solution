5e1bbafd56f3ea9ea2223e8bcf5099ed6581b17a
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-06 21:39:17 +0530

    Added MachinesController and the Index View for View Method of MachinesControoler

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,21 @@
+﻿using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Factory.Controllers
+{
+    public class MachinesController : Controller
+    {
+        public static List<Machine> Machines = new()
+        {
+            new Machine(1, "Dreamweaver", "A machine that creates dreams."),
+            new Machine(2, "Bubblewrappinator", "A machine that wraps everything in bubbles."),
+            new Machine(3, "Laughbox", "A machine that generates endless laughter.")
+        };
+
+        public IActionResult Index()
+        {
+            return View(Machines);
+        }
+    }
+}
