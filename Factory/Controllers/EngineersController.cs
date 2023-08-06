542cf1e27a8b33c325f31d941f8bc63ac6b23317
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-08-06 15:35:02 +0530

    Added more properties in Engineer Modal

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -6,9 +6,9 @@ namespace Factory.Controllers
     public class EngineersController : Controller
     {
 
-        private static List<Engineer> Engineers = new (){ new Engineer("Raed", "ASDU1312736", 1), new Engineer("Ammar", "YAGD12837912", 2) };
+        private static List<Engineer> Engineers = new() { new Engineer("Raed", "raed@example.com", "123456789", "ASDU1312736", 1), new Engineer("Ammar", "ammar@example.com", "555777666", "YAGD12837912", 2) };
 
-       public IActionResult Index()
+        public IActionResult Index()
         {
 
             return View(Engineers);
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -5,15 +5,19 @@ namespace Factory.Models
     public class Engineer
     {
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }    
         public string LicenseID{ get; set; }
         public int EngineerID { get; set; }
         public List<Machine> Machines { get; set; }
 
 
-        public Engineer(string Name, string licenseID, int engineerID)
+        public Engineer(string Name, string email, string phone, string licenseID, int engineerID)
         {
 
             this.Name = Name;
+            this.Email = email;
+            this.Phone = phone;
             this.Machines = new();
             this.LicenseID = licenseID;
             this.EngineerID = engineerID;
